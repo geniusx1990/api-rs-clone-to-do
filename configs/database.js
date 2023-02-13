d8995f1a084bdbad5f9a6c5b6efbb6ef8ce02ab1
geniusx1990 <geniusx1990@gmail.com>
2023-02-14 02:19:11 +0300

    fix: added credentials for product testing

--- configs/database.js
+++ configs/database.js
@@ -1,11 +1,20 @@
 const { Client } = require("pg");
 const client = new Client({
     user: 'me',
-    host: 'localhost',
-    database: 'red',
-    password: 'password',
+    host: 'dpg-cflc8i9gp3ju5h3brhu0-a',
+    database: 'api_k4e1',
+    password: '8WceEYPDa6QbSWtZZibWnG72haIjsAeq',
     port: 5432,
 })
 
 
 module.exports = client;
+
+
+/* 
+for testing 
+user: 'me',
+host: 'localhost',
+database: 'red',
+password: 'password',
+port: 5432, */
